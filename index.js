e6cec897720e815e738a42fb0d9ec03b670720a4
Elizabeth Witengier <ewitengier@ufl.edu>
2022-06-13 16:30:34 -0400

    still stuck

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-var form = document.getElementsByClassName('.todo')
-var list = document.getElementById('todo-list')
-var input = document.querySelector('input')
+var form = document.getElementById('add-todo')
+var ul = document.getElementById('todo-list')
+
+
 
 form.onsubmit = function(e) {
   e.preventDefault()
-  var input = this.input.value
-  this.input.value = ""
-  add
+  var input = document.querySelector('input').value
+  input.value = ""
+  console.log(input)
 
-function addEventListener() {
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode("Four"));
-  list.appendChild(li);
-  
-}  
-  
-function myFunction(){
-  if(inputLength() > 0){
-    let li = document.createElement("li");
-    li.setAttribute("id","myLi");
-    var text = document.createTextNode(input.value);
-      
-    const deleteButton = document.createElement('button')
-    deleteButton.innerText = 'Delete'
-  
-    deleteButton.addEventListener('click', event => list.removeChild(li))
-  
-    li.appendChild(deleteButton)
-      list.appendChild(li);
-      li.appendChild(text);
-      input.value="";
-    }
+ 
+  function addEventListener() {
+  var list = document.createElement("li");
+  li.appendChild(document.createTextNode("input"));
+  list.position(ul);
   }
 }
-
 
 
 
