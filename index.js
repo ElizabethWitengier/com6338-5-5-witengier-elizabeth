02e7c69b7afb15a83164771a54d062e2bc391df7
Elizabeth Witengier <ewitengier@ufl.edu>
2022-06-16 15:53:25 -0400

    still trying to solve

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var form = document.getElementById('add-todo')
 var ul = document.getElementById('todo-list')
-
+var button = document.querySelector('button')
 
 
 form.onsubmit = function(e) {
   e.preventDefault()
   var input = document.querySelector('input').value
+  var list = document.createElement('li')
+  list.innerHTML = input.value
+  ul.appendChild(list);
   input.value = ""
-  console.log(input)
-
- 
-  function addEventListener() {
-  var list = document.createElement("li");
-  li.appendChild(document.createTextNode("input"));
-  list.position(ul);
-  }
+  console.log(list)
+  list.addEventListener('click', function() {
+    list.style.textDecoration = "line-through"
+  })
+  list.addEventListener('dblclick', function() {
+    ul.removeChild(list)
+  })
 }
+
 
 
 
