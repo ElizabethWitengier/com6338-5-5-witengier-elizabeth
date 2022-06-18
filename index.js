f22d484101bc3af6a6d10a094da20ad496bcef20
Elizabeth Witengier <ewitengier@ufl.edu>
2022-06-17 20:03:52 -0400

    final final

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 var form = document.getElementById('add-todo')
-var ul = document.getElementById('todo-list')
-var button = document.querySelector('button')
-
 
 form.onsubmit = function(e) {
   e.preventDefault()
-  var input = document.querySelector('input')
-  var list = document.createElement('li')
-  list.innerHTML = input.value
-  ul.appendChild(list)
-  input.value = ""
-  console.log(list)
   
-  list.addEventListener('click', function() {
-    list.style.textDecoration = "line-through"
+  var input = document.querySelector('input')
+  var ul = document.getElementById('todo-list')
+  var button = document.createElement('button')
+  var li = document.createElement('li')
+  var inputText = document.createTextNode(input.value)  
+  
+
+  input.value = ""
+  button.appendChild(inputText)
+  li.appendChild(button)
+  ul.appendChild(li)
+  console.log(ul)
+  
+button.addEventListener('click', function() {
+  button.style.textDecoration = "line-through"
   })
-  list.addEventListener('dblclick', function() {
-    ul.removeChild(list)
+button.addEventListener('dblclick', function() {
+  ul.removeChild(li)
   })
 }
 
